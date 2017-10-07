062a0c6882537120d43c10479d352a2d5102b9a6
Joel Romero <joelrg1288@gmail.com>
2017-10-07 16:40:18 -0500

    Se completa la práctica 1 correctamente enviando archivos, carpetas y subcarpetas.

---
+++ Prácticas/Práctica1/Manejador Archivos/src/ArchivosCliente.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.*;
+import java.net.*;
+public class ArchivosCliente 
+{
+    private DataOutputStream buferSalida;
+    private DataInputStream buferEntrada;
+    private Socket cliente;
+    private String host = "127.0.0.1", nombreArch, ruta;
+    private int i = 0, puerto = 9876, porcentaje, n;
+    private long enviados, tamanioArchivo;
+    
+
+    public ArchivosCliente (File [] archivo)
+    {
+            try
+            {
+                while (i < archivo.length)
+                {
+                        nombreArch = (archivo [i]).getName ();
+                        ruta = (archivo [i]).getAbsolutePath ();
+                        tamanioArchivo = (archivo [i]).length ();
+                        if ((archivo [i]).isFile ())
+                                envia (archivo [i], nombreArch, ruta, tamanioArchivo, 'a', "");
+                        else
+                                envia (archivo [i], nombreArch, ruta, tamanioArchivo, 'd', nombreArch);
+                        i ++;
+                }
+                System.out.println ("\n\nArchivo enviado correctamente.\n");
+                buferSalida.close ();
+                buferEntrada.close ();
+                cliente.close ();
+            }catch (Exception ex)
+            {
+            ex.printStackTrace ();
+            }
+    }
+
+    public void envia (File archivo, String nombreArch, String ruta, long tamanioArchivo, char tipo, String DirPadre)
+    {
+            try
+            {
+                    cliente = new Socket (host, puerto);
+                    buferSalida = new DataOutputStream (cliente.getOutputStream ());
+                    buferSalida.writeChar (tipo);
+                    buferSalida.flush ();
+                    buferSalida.writeUTF (nombreArch);
+                    buferSalida.flush ();
+                    buferSalida.writeUTF (DirPadre);
+                    buferSalida.flush ();
+                    if (tipo == 'a')														//Si se va a copiar un archivo
+                    {
+                            System.out.println ("\n\nInicia la transferencia del archivo " + nombreArch);
+                            enviados = 0;
+                            porcentaje = 0;
+                            buferEntrada = new DataInputStream (new FileInputStream (ruta));
+                            buferSalida.writeLong (tamanioArchivo);
+                            buferSalida.flush ();
+                            while (enviados < tamanioArchivo)
+                            {
+                                    byte [] b = new byte [1500];
+                                    n = buferEntrada.read (b);
+                                    buferSalida.write (b, 0, n);							//Escribimos desde el byte 0 hasta el byte n
+                                    buferSalida.flush ();
+                                    enviados += n;
+                                    porcentaje = (int)((enviados * 100) / tamanioArchivo);
+                                    System.out.print ("\rEnviados: " + porcentaje + " %");
+                            }
+                    }else
+                    {
+                            System.out.println ("\n\nInicia la transferencia del directorio " + nombreArch);
+                            File [] directorio = archivo.listFiles ();
+                            int i = 0;
+                            while (i < directorio.length)
+                            {
+                                    nombreArch = (directorio [i]).getName ();
+                                    ruta = (directorio [i]).getAbsolutePath ();
+                                    tamanioArchivo = (directorio [i]).length ();
+                                    if ((directorio [i]).isFile ()){
+                                            //System.out.println("ENVIANDO COMO RUTA  "+ DirPadre);
+                                            envia (directorio [i], nombreArch, ruta, tamanioArchivo, 'a', DirPadre);
+                                    }
+                                    else{
+                                            //System.out.println("ENVIANDO COMO RUTA  "+ DirPadre+"\\"+nombreArch);
+                                            envia (directorio [i], nombreArch, ruta, tamanioArchivo, 'd', DirPadre+"\\"+nombreArch);
+                                    }
+                                    i ++;
+                            }
+                    }
+                    buferSalida.close ();
+                    buferEntrada.close ();
+                    cliente.close ();
+            }catch (Exception e)
+            {
+                    e.printStackTrace ();
+            }
+    }
+}
---
+++ Prácticas/Práctica1/Manejador Archivos/src/ArchivosServidor.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Natalia
+ */
+import java.net.*;
+import java.io.*;
+
+public class ArchivosServidor
+{
+	private ServerSocket servidor;
+	private Socket cliente;
+	private int puerto = 9876, n = 0, porcentaje = 0;
+	private DataInputStream dis;
+	private DataOutputStream dos;
+	private String nombre,DirPadre;
+	private long tam, r;
+	private char tipo;
+        String rutaS="C:\\Users\\enata_000\\Documents\\SERVIDOR\\";
+        String carpeta="";
+
+	public ArchivosServidor ()
+	{
+		try
+		{
+			servidor = new ServerSocket (puerto);
+                        servidor.setReuseAddress(true);
+			System.out.println ("Servicio iniciado... esperando clientes.\n\n");
+			for (;;)
+			{
+				cliente = servidor.accept ();
+				System.out.println ("Cliente conectado desde -> " + cliente.getInetAddress () + ":" + cliente.getPort());
+				recibe (rutaS);
+			}
+		}catch (Exception ex)
+		{
+			ex.printStackTrace ();
+		}
+	}
+
+	public void recibe (String ruta)
+	{
+		r = 0;
+		try
+		{
+			dis = new DataInputStream (cliente.getInputStream ());
+			tipo = dis.readChar ();
+			nombre = dis.readUTF ();
+                        DirPadre = dis.readUTF ();
+                        
+			if (tipo == 'a')
+			{
+                            
+				tam = dis.readLong ();
+				if(DirPadre==""){
+                                    System.out.println ("Recibiendo archivo " + nombre +"        RUTA DESTINO "+ruta+DirPadre+nombre);
+                                    //System.out.println ("Recibiendo archivo " + nombre +"       Dirpadre> "+DirPadre + "        ruta "+ruta+DirPadre+nombre);
+                                    dos = new DataOutputStream (new FileOutputStream (ruta+DirPadre+nombre));
+                                }
+                                else{
+                                    System.out.println ("Recibiendo archivo " + nombre +"        RUTA DESTINO "+ruta+DirPadre+"\\"+nombre);
+                                    //System.out.println ("Recibiendo archivo " + nombre +"       Dirpadre> "+DirPadre + "        ruta "+ruta+DirPadre+"\\"+nombre);
+                                    dos = new DataOutputStream (new FileOutputStream (ruta+DirPadre+"\\"+nombre));
+                                }
+				while (r < tam)
+				{
+					byte [] b = new byte [1500];
+					n = dis.read (b);
+					dos.write (b, 0, n);
+					dos.flush ();
+					r += n;
+					porcentaje = (int) ((r * 100) / tam);
+					System.out.print ("\rRecibido el " + porcentaje + "%");
+				}
+				System.out.println ("\nArchivo recibido...\n\n");
+                        
+			}else
+			{
+				System.out.println ("Recibiendo directorio " + nombre + "        RUTA DESTINO "+ruta+DirPadre);
+				//System.out.println ("Recibiendo directorio " + nombre +"Dirpadre> "+DirPadre+ "        ruta "+ruta+DirPadre);
+				boolean bol = new File (ruta+DirPadre+"\\").mkdir ();
+                                System.out.println ("Directorio creado...\n\n");
+			}
+			dos.close ();
+			dis.close ();
+			cliente.close ();
+		}catch (Exception e)
+		{
+			e.printStackTrace ();
+		}
+	}
+	public static void main(String[] args)
+	{
+		new ArchivosServidor ();
+	}
+        
+}
